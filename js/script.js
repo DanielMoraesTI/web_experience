03423c0e4ff335ad95df1d88655c27a5e9a31482
Daniel Fernandes de Moraes <ti.danielmoraes@gmail.com>
2025-12-26 13:17:31 +0000

    Base para o prijeto de final de módulo

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const dia = document.getElementById("dia");
+const eventos = document.querySelectorAll(".evento");
+
+dia.addEventListener("change", () => {
+    const escolha = dia.value;
+
+    eventos.forEach(evento => {
+        if(escolha === "todos" || evento.dataset.dia === escolha) {
+            evento.style.display = "block";
+        } else {
+            evento.style.display = "none";
+        }
+    });  
+});
