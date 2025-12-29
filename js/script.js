1407c229ab6246b4f40d5484a8ae2cc6fa6da81b
Daniel Fernandes de Moraes <ti.danielmoraes@gmail.com>
2025-12-29 14:58:13 +0000

    Acrescentado um Modal para pesquisa de motivação a ir ao evento quando da conirmação da compra do bilhete

--- js/script.js
+++ js/script.js
@@ -22,3 +22,18 @@ function responder(event) {
     event.preventDefault();
     alert("Obrigado pelo contacto, iremos responder em até 48 horas");
 };
+
+const abrirModal = document.getElementById("comprar");
+const fecharModal = document.getElementById("fechar-modal");
+const modal = document.getElementById("modalPesquisa");
+
+abrirModal.addEventListener("click", () => {
+    modal.style.display = "flex";
+    modal.setAttribute("aria-hidden", "false");
+});
+
+fecharModal.addEventListener("click", () => {
+    modal.style.display = "none";
+    modal.setAttribute("aria-hidden", "true");
+    alert("Obrigado pela sua contribuição na pesquisa!");
+});
